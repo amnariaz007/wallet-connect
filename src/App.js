a9a876c28bfcfc285d1d23652f3fefbbf6d736c5
Amna <amna.riaz@invozone.com>
2023-05-23 16:21:16 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet, polygon, polygonMumbai, sepolia, gnosis } from 'wagmi/chains'
+import HomePage from './HomePage'
 
-function App() {
+const chains = [arbitrum, polygon, polygonMumbai, sepolia, gnosis]
+const projectId = 'YOUR_PROJECT_ID'
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1 , chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <WagmiConfig config={wagmiConfig}>
+        <HomePage />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  )
 }
 
-export default App;
+ 
---
+++ src/HomePage.js
@@ -0,0 +1,7 @@
+import { Web3Button } from '@web3modal/react'
+
+function HomePage() {
+  return <Web3Button />
+}
+
+export default HomePage;
